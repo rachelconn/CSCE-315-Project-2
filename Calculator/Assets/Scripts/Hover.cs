3092ec08d8384e693a8135b9d47cf3622af8dd36
Kelvin Wang <30770221+kelvinwop@users.noreply.github.com>
2019-10-23 17:25:23 -0500

    stuff

--- Calculator/Assets/Scripts/Hover.cs
+++ Calculator/Assets/Scripts/Hover.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    
+    bool hovering;
+    float hoverSecs;
+
+    public readonly float HOVER_TIME_CUTOFF = 2;
+
+    void Start()
+    {
+        hovering = false;
+        hoverSecs = 0f;
+    }
+
+    void Update()
+    {
+        if (hovering)
+        {
+            hoverSecs += Time.deltaTime;
+        }
+        else
+        {
+            hoverSecs = 0f;
+        }
+
+        if (hoverSecs > HOVER_TIME_CUTOFF)
+        {
+            buttonClicked();
+            hoverSecs = 0f;
+        }
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        hovering = true;
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        hovering = false;
+    }
+
+    void buttonClicked()
+    {
+        throw new System.Exception("lol");
+    }
 }
