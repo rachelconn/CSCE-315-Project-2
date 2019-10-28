57b3e70ad504e223caaed40b12ae5e22afb997eb
Ryan Conn <rconn478@gmail.com>
2019-10-29 00:28:19 +0100

    Remade the logic stuff

--- Calculator/Assets/Scripts/CalculatorDisplayController.cs
+++ Calculator/Assets/Scripts/CalculatorDisplayController.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class CalculatorDisplayController : MonoBehaviour
 {
     TextMeshProUGUI textComponent;
+    static readonly char[] symbols ={'×','÷','+','-'};
     void Start()
     {
         textComponent = gameObject.GetComponent<TextMeshProUGUI>();
@@ -27,7 +28,7 @@ public class CalculatorDisplayController : MonoBehaviour
     private static string ToTilda(string equation)
     {
         string retval = "";
-        char[] mdas = { '×', '÷', '+', '-' };
+        char[] mdas = { '*', '/', '+', '-' };
 
         // Loop through the string.
         for (int i = 0; i < equation.Length; ++i)
@@ -45,125 +46,42 @@ public class CalculatorDisplayController : MonoBehaviour
         return retval;
     }
 
+    public static string Add(string param) {
+        if (!param.Contains("+")) {
+            return Subtract(param);
+        }
+        string[] result = param.Split(new char[] {'+'}, 2);
+        return (decimal.Parse(result[0]) + decimal.Parse(Add(result[1]))).ToString();
+    }
+
+    public static string Subtract(string param) {
+        if (!param.Contains("-")) {
+            return param;
+        }
+        string[] result = param.Split(new char[] {'-'}, 2);
+        return (decimal.Parse(result[0]) - decimal.Parse(Subtract(result[1]))).ToString();
+
+    }
+    public static string Multiply(string param) {
+        if (!param.Contains("×")) {
+            return Divide(param);
+        }
+        string[] result = param.Split(new char[] {'×'}, 2);
+        return (decimal.Parse(result[0]) * decimal.Parse(Multiply(result[1]))).ToString();
+    }
+
+    public static string Divide(string param) {
+        if (!param.Contains("÷")) {
+            return Add(param);
+        }
+        string[] result = param.Split(new char[] {'×'}, 2);
+        return (decimal.Parse(result[0]) / decimal.Parse(Divide(result[1]))).ToString();
+    }
+
     public static void Evaluate()
     {
-        string textToEvaluate = CalculatorInputManager.inputSequence;
-        Debug.Log(textToEvaluate);
-        //Set up the variables 
-        textToEvaluate = ToTilda(textToEvaluate);
-        char[] md = { '×', '÷' };
-        char[] mdas = { '×', '÷', '+', '~' };
-        int i;
-        string front;
-        string back;
-        string opFront;
-        string opBack;
-        string temp = "";
-        int j;
-        int k = 0;
-        string operateOn = "";
-        float equals;
-
-        // Loop until there are no more operators.
-        while (textToEvaluate.IndexOfAny(mdas) != -1)
-        {
-            // Find the index of the next operator.
-            if (textToEvaluate.IndexOfAny(md) != -1)
-            {
-                i = textToEvaluate.IndexOfAny(md);
-            }
-            else
-            {
-                i = textToEvaluate.IndexOfAny(mdas);
-            }
-
-            // Split the string where the operator was found.
-            if (i > 1)
-            {
-                front = textToEvaluate.Substring(0, i - 1);
-            }
-            else
-            {
-                front = textToEvaluate[0].ToString();
-            }
-            back = textToEvaluate.Substring(i + 1);
-
-            // If that wasn't the only operator in the string, then go through the the if statement.
-            if (textToEvaluate.IndexOfAny(mdas) != textToEvaluate.LastIndexOfAny(mdas))
-            {
-                // If there is an operator in the front half, then stop set the cut off everything after the operator.
-                // Otherwise, set front to empty.
-                j = front.LastIndexOfAny(mdas);
-                if (j != -1)
-                {
-                    front = front.Substring(0, j + 1);
-                }
-                else
-                {
-                    front = "";
-                }
-
-
-                // If there is an operator in the back half, then stop set the cut off everything after the operator.
-                // Otherwise, set back and temp to empty.
-                if (back.IndexOfAny(mdas) != -1)
-                {
-                    k = back.IndexOfAny(mdas);
-                    temp = back[k].ToString();
-                    k = textToEvaluate.IndexOf(back) + k;
-                    back = back.Substring(back.IndexOfAny(mdas) + 1);
-                }
-                else
-                {
-                    k = textToEvaluate.Length;
-                    back = "";
-                    temp = "";
-                }
-
-                // This is the part of the equation that will be operated on.
-                // It should only consist of 2 operands and 1 operator.
-                operateOn = textToEvaluate.Substring(j + 1, k - j - 1);
-            }
-            else
-            {
-                // This should only be run whenever the equation only has one operator.
-                front = "";
-                back = "";
-                temp = "";
-                operateOn = textToEvaluate;
-            }
-
-            // Split operateOn to get the two operands.
-            i = operateOn.IndexOfAny(mdas);
-            opFront = operateOn.Substring(0, i);
-            opBack = operateOn.Substring(i + 1);
-
-            // Perform the operation
-            if (operateOn.Contains("*"))
-            {
-                equals = float.Parse(opFront) * float.Parse(opBack);
-            }
-            else if (operateOn.Contains("/"))
-            {
-                equals = float.Parse(opFront) / float.Parse(opBack);
-            }
-            else if (operateOn.Contains("+"))
-            {
-                equals = float.Parse(opFront) + float.Parse(opBack);
-            }
-            else
-            {
-                equals = float.Parse(opFront) - float.Parse(opBack);
-            }
-
-            // Insert the result back into the equation before looping back again
-            textToEvaluate = front + equals.ToString() + temp + back;
-            Debug.Log(textToEvaluate);
-            Debug.Log(equals);
-
-        }
-        Debug.Log(textToEvaluate);
-        // just do it ✔
-        CalculatorInputManager.inputSequence = textToEvaluate;
+        string input = CalculatorInputManager.inputSequence;
+        //char[] symbols ={'×','÷','+','-'};
+        CalculatorInputManager.inputSequence = Multiply(input);
     }
 }
